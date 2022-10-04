666a54d34fe33e45855b83125b20d94b0ed71a62
YI NIAN NIU <yinian@login01.sockeye.arc.ubc.ca>
2022-10-04 15:10:20 -0700

    Preliminary work on replicating a deep model

---
+++ deep-models/cajal.py
@@ -0,0 +1,179 @@
+import os
+os.environ[ 'NUMBA_CACHE_DIR' ] = '/tmp/' # https://github.com/scverse/scanpy/issues/2113
+from os.path import join
+
+import logging
+import anndata as ad
+import pickle
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import scipy
+
+import h5py
+import hdf5plugin
+import tables
+
+from sklearn.preprocessing import binarize
+
+
+def atac_de_analysis(adata):
+    '''get top DA peaks per cell type'''
+    adata.X = binarize(adata.X)
+    sc.tl.rank_genes_groups(adata, 'cell_type', method='t-test')
+    cell_types = adata.obs.cell_type.value_counts().index
+    column_names = ['names', 'scores', 'logfoldchanges', 'pvals', 'pvals_adj', 'cell_type']
+    df = pd.DataFrame(columns = column_names)
+    for cell_type in cell_types:
+        dedf = sc.get.rank_genes_groups_df(adata, group=cell_type)
+        dedf['cell_type'] = cell_type
+        dedf = dedf.sort_values('scores', ascending=False).iloc[:100]
+        df = df.append(dedf, ignore_index=True)
+    return df
+
+
+def gex_de_analysis(adata_GEX):
+    '''get top DE genes per cell type (multiome)'''
+    sc.pp.filter_cells(adata_GEX, min_genes=200)
+    sc.pp.filter_genes(adata_GEX, min_cells=3)
+    adata_GEX.var['mt'] = adata_GEX.var_names.str.contains('MT-') 
+    sc.pp.calculate_qc_metrics(adata_GEX, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    adata_GEX = adata_GEX[adata_GEX.obs.n_genes_by_counts < 4000, :]
+    sc.pp.normalize_total(adata_GEX, target_sum=1e4)
+    sc.pp.log1p(adata_GEX)
+    sc.pp.highly_variable_genes(adata_GEX, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    sc.pp.scale(adata_GEX, max_value=10)
+    sc.tl.rank_genes_groups(adata_GEX, 'cell_type', method='wilcoxon')
+    cell_types = adata_GEX.obs.cell_type.value_counts().index
+    column_names = ['names', 'scores', 'logfoldchanges', 'pvals', 'pvals_adj', 'cell_type']
+    df = pd.DataFrame(columns = column_names)
+    for cell_type in cell_types:
+        dedf = sc.get.rank_genes_groups_df(adata_GEX, group=cell_type)
+        dedf['cell_type'] = cell_type
+        dedf = dedf.sort_values('scores', ascending=False).iloc[:100]
+        df = df.append(dedf, ignore_index=True)
+    return df
+
+
+class Hyperparameters:
+    #class to store hyperparameters
+    def __init__(self, dropout, layer_shapes):
+        self.dropout = dropout
+        self.layer_shapes = layer_shapes
+        self.n_layers = len(layer_shapes)
+
+
+class Cajal(nn.Module):
+    def __init__(self, hp, input_shape, output_shape, min_val, max_val):
+        super(Cajal, self).__init__()
+        self.name = 'Cajal'
+        modules = [
+            nn.Dropout(hp.dropout),
+            nn.Linear(input_shape, hp.layer_shapes[0]),
+            nn.ReLU()
+        ]
+        for i in range(hp.n_layers - 1):
+            modules.append(nn.Linear(hp.layer_shapes[i], hp.layer_shapes[i+1]))
+            modules.append(nn.ReLU())
+        modules.append(nn.Linear(hp.layer_shapes[-1], output_shape))
+        self.stack = nn.Sequential(*modules)
+        self.lambd = lambda x: torch.clamp(x, min_val, max_val)
+    
+    def forward(self, x):
+        x = self.stack(x)
+        return self.lambd(x)
+
+
+class CajalWrapper:
+    '''
+    Wrapper for the neural network from Team Cajal
+    '''
+    def __init__(self, config):
+        self.type = config['type']
+        self.config = config
+
+
+    def preprocess(self, x_train: ad.AnnData, x_test: ad.AnnData, y: ad.AnnData):
+        if self.type == 'multiome':
+            genes = atac_de_analysis(x_train.copy())
+            genes.to_csv(join(self.config['output_dir'], 'DEGs.csv'))
+            selected_genes = set(genes.names)
+        else:
+            genes1 = gex_de_analysis(x_train.copy())
+            genes1.to_csv(join(self.config['output_dir'], 'DEGs.csv'))
+            selected_genes = set(genes1.names).union(y.var_names)
+        
+        self.min_val = np.min(y.X)
+        self.max_val = np.max(y.X)
+
+        train_total = np.sum(x_train.X.toarray(), axis=1)
+        test_total = np.sum(x_test.X.toarray(), axis=1)
+
+        subset = selected_genes.intersection(x_train.var_names)
+        x_train = x_train[:, list(subset)]
+
+        with open(join(self.config['output_dir'], 'genes.pkl'), "wb") as out:
+            pickle.dump(x_train.var_names, out, -1)
+        
+        x_train_final = x_train.X.toarray()
+        y_final = y.X.toarray()
+        x_test_final = x_test.X.toarray()
+
+        train_batches = set(x_train.obs.donor)
+        x_train.obs["batch_median"] = 0
+        x_train.obs["batch_sd"] = 0
+        for batch in train_batches:
+            x_train.obs["batch_median"][x_train.obs.donor == batch] = np.median(train_total[x_train.obs.donor == batch])
+            x_train.obs["batch_sd"][x_train.obs.donor == batch] = np.std(train_total[x_train.obs.donor == batch])
+
+        test_batches = set(x_test.obs.donor)
+        x_test.obs["batch_median"] = 0
+        x_test.obs["batch_sd"] = 0
+
+        for batch in test_batches:
+            x_test.obs["batch_median"][x_test.obs.donor == batch] = np.median(test_total[x_test.obs.donor == batch])
+            x_test.obs["batch_sd"][x_test.obs.donor == batch] = np.std(test_total[x_test.obs.donor == batch])
+        
+        for i in range(50):
+            x_train_final = np.column_stack((X_train,train_total))
+        for i in range(50):
+            x_train_final = np.column_stack((X_train,x_train.obs["batch_median"]))
+        for i in range(50):
+            x_train_final = np.column_stack((X_train,x_train.obs["batch_sd"]))
+
+        for i in range(50):
+            x_test_final = np.column_stack((x_test_final,test_total))
+        for i in range(50):
+            x_test_final = np.column_stack((x_test_final,x_test.obs["batch_median"]))
+        for i in range(50):
+            x_test_final = np.column_stack((x_test_final,x_test.obs["batch_sd"]))
+        
+        x_train_final = x_train_final.T
+        means = np.mean(x_train_final, axis = 1)
+        sds = np.std(x_train_final, axis = 1)
+        means = means.reshape(len(means), 1)
+        sds = sds.reshape(len(sds), 1)
+        info = {"means":means,"sds":sds}
+
+        with open(join(self.config['output_dir'], "./transformation.pkl"), "wb") as out:
+            pickle.dump(info, out, -1)
+        
+        x_train_final = (x_train_final - means)/sds
+        x_train_final = x_train_final.T
+
+        x_test_final = x_test_final.T
+        x_test_final = (x_test_final - info["means"])/info["sds"]
+        x_test_final = x_test_final.T
+
+        self.input_shape = x_train_final.shape[1]
+        self.output_shape = y_final.shape[1]
+
+        return x_train_final, x_test_final, y_final
+
+    def initialize(self) -> Cajal:
+        """
+        Setup a Cajal model using the hyperparameters in <config>
+        """
+        params = config['model_params']
+        hp = Hyperparameters(params['dropout'], params['hidden_layers'])
+        return Cajal(hp, self.input_shape, self.output_shape, self.min_val, self.max_val)
---
+++ deep-models/run.py
@@ -0,0 +1,180 @@
+"""To run this script, sample command
+python run.py --path=<insert-yaml-path>
+"""
+import argparse
+import gc
+import logging
+import pickle
+from datetime import datetime
+from os import makedirs
+from os.path import dirname, join
+from pathlib import Path
+
+import numpy as np
+import anndata as ad
+import scipy
+import yaml
+from sklearn.decomposition import TruncatedSVD
+from sklearn.model_selection import KFold
+from utils import *
+
+import torch
+import torch.nn as nn
+
+
+cuda = torch.cuda.is_available()
+
+
+def main(config):
+    # part of an HPO trial
+    trial = config.get("trial", None)
+
+    # Load Data
+    logging.info("Loading data")
+    # x_train_transformed = pickle.load(open(config["paths"]["x"], "rb"))
+    # x_test_transformed = pickle.load(open(config["paths"]["x_test"], "rb"))
+
+    # load y as it is, since we need the original values to get metrics
+    # y = scipy.sparse.load_npz(config["paths"]["y"])
+
+    adatas = load_data_as_anndata(config['paths'], config['metadata'])
+    x_train_transformed = adatas['x']
+    x_test_transformed = adatas['x_test']
+    y = adatas['y']
+
+    # Setup model
+    logging.info("Setting up the model")
+    model = setup_model(config)
+
+    # perform preprocessing
+    logging.info("Performing Preprocessing on x, y")
+    if config["preprocessing"] == "Custom":
+        x_train_transformed, x_test_transformed, y_transformed = model.preprocess(x_train_transformed, x_test_transformed, y)
+    elif config["preprocessing"] == "TruncatedSVD":
+        # transform y
+        pca_y = TruncatedSVD(
+            n_components=config["preprocessing_params"]["output_dim"],
+            random_state=config["seed"],
+        )
+        y_transformed = pca_y.fit_transform(y)
+    else:
+        raise NotImplementedError
+    gc.collect()
+
+    # perform KFold cross validation
+    logging.info("Setting up cross validation")
+    np.random.seed(config["seed"])
+    all_row_indices = np.arange(x_train_transformed.shape[0])
+    np.random.shuffle(all_row_indices)
+
+    kf = KFold(n_splits=5, shuffle=True, random_state=config["seed"])
+
+    # Save the model (do not save when config['save_models'] == False)
+    save_models = config.get("save_models", True)
+
+    # for cross val scores
+    scores = []
+
+    # for x_test predictions
+    predictions = []
+    for i, (tr_indices, val_indices) in enumerate(kf.split(all_row_indices)):
+        # preparing ith fold, for y_val we will use (not)transformed vector to calculate scores
+        logging.info(f"{i}th fold")
+        x_train, y_train = (
+            x_train_transformed[tr_indices, :],
+            y_transformed[tr_indices, :],
+        )
+        x_val, y_val = x_train_transformed[val_indices, :], y[val_indices, :].toarray()
+
+        # initialize an instance of the model
+        net = model.initialize()
+        if cuda:
+            net.to('cuda')
+        net.train()
+
+        # fit and then delete training splits
+        logging.info(f"Fitting the model for {i}th Fold")
+        # model.fit(x_train, y_train)
+        train(net, config, save_models, x_train, y_train)
+        del x_train, y_train
+
+        # save models if save_models=True
+        # if save_models:
+        #     if trial is not None:
+        #         pkl_filename = join(
+        #             config["output_dir"], f"trial_{trial}", f"model_{i}th_fold.pkl"
+        #         )
+        #     else:
+        #         pkl_filename = join(config["output_dir"], f"model_{i}th_fold.pkl")
+        #     with open(pkl_filename, "wb") as file:
+        #         pickle.dump(model, file)
+
+        logging.info("Predicting and calculating metrics")
+        net.eval()
+        scores.append(
+            correlation_score(y_val, net(x_val))
+        )
+
+        del x_val, y_val
+        gc.collect()
+
+        # Perform test predictions with cross val model
+        predictions.append(net(x_test_transformed))
+        if i == 1:
+            break
+
+    # Again garbage collection to reduce unnecessary memory usage
+    gc.collect()
+
+    logging.info(f"Scores on 5-fold CV: {scores}")
+
+    # post processing final predictions before pickle saving them
+    if config.get("prediction_agg", "") == "mean":
+        # mean over cross val predictions
+        prediction = np.mean(predictions, axis=0)
+    else:
+        raise NotImplementedError
+
+    # save numpy array
+    if config.get("save_test_predictions", True):
+        if trial is not None:
+            pkl_filename = join(
+                config["output_dir"], f"trial_{trial}", f"test_pred.pkl"
+            )
+        else:
+            pkl_filename = join(config["output_dir"], f"test_pred.pkl")
+        logging.info(f"Saving Predictions to {pkl_filename}")
+        makedirs(dirname(pkl_filename), exist_ok=True)
+        with open(pkl_filename, "wb") as file:
+            pickle.dump(prediction, file)
+
+    # return mean of cross val scores
+    return np.mean(scores)
+
+
+if __name__ == "__main__":
+    # take experiment config input
+    parser = argparse.ArgumentParser(description="Input config path")
+    parser.add_argument(
+        "--path", type=str, required=True, help="Path of the experiment config"
+    )
+    args = parser.parse_args()
+
+    config = yaml.safe_load(Path(args.path).read_text())
+
+    # Setup output directory
+    config["output_dir"] = join(
+        config["output_dir"], datetime.now().strftime("%d_%m_%Y-%H_%M")
+    )
+    makedirs(config["output_dir"])
+    logging.basicConfig(
+        filename=join(config["output_dir"], config.get("log_dir", "output.log")),
+        filemode="a",
+        level=logging.INFO,
+    )
+
+    # log the config
+    logging.info(f'Configuration: {config}')
+
+    # run main with config inputted
+    main(config)
