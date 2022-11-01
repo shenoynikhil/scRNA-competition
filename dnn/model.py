e0a0e640c17896e96781b43ded3f3f374fc8d6a8
Nikhil Shenoy <shenoy@login01.sockeye.arc.ubc.ca>
2022-10-31 19:36:40 -0700

    Removed utils file

--- dnn/model.py
+++ dnn/model.py
@@ -114,12 +114,7 @@ class DNNSetup:
         logging.info(scores)
         return np.mean(scores)
 
-    def conduct_hpo(
-        self,
-        subset_size: int = 5000,
-        n_trials: int = 10,
-        train_subset_frac: float = 0.8,
-    ):
+    def conduct_hpo(self, n_trials: int = 10):
         """Conduct Hyperparameter Optimization using Optuna"""
         # get datamodule
         datamodule = self.setup_datamodule(self.config.get("datamodule_config"))
@@ -163,3 +158,42 @@ class DNNSetup:
 
         # logging best results
         logging.info(f"Best results: {study.best_trial}")
+
+
+def update_config(model_config, trial):
+    """Function for updating experiment config accordingly"""
+    # create a copy of the config to be returned in the end
+    model_type = model_config.get("model_type", "BaseNet")
+    if model_type == "BaseNet":
+        model_config.update(
+            {
+                "mse_weight": trial.suggest_categorical(
+                    "mse_weight", [0.1 * x for x in range(11)]
+                ),
+                "pcc_weight": trial.suggest_categorical(
+                    "mse_weight", [0.1 * x for x in range(11)]
+                ),
+                "dropout": trial.suggest_categorical(
+                    "dropout", [0.1 * x for x in range(5)]
+                ),
+            }
+        )
+    elif model_type == "ContextConditioningNet":
+        model_config.update(
+            {
+                "mse_weight": trial.suggest_categorical(
+                    "mse_weight", [0.1 * x for x in range(11)]
+                ),
+                "pcc_weight": trial.suggest_categorical(
+                    "mse_weight", [0.1 * x for x in range(11)]
+                ),
+                "dropout": trial.suggest_categorical(
+                    "dropout", [0.1 * x for x in range(5)]
+                ),
+                "beta": trial.suggest_float(
+                    "beta", low=0.0001, high=1.0, step=0.0001, log=True
+                ),
+            }
+        )
+
+    return model_config
